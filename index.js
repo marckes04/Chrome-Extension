00e119f223df6366c0b3426d6c302695bc40634f
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2025-12-15 00:33:05 +0100

    Added Files

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click",function(){
+    console.log("Button clicked from addEventListener");
+}) 
+{
+    console.log("Button clicked from onclick attribute");
