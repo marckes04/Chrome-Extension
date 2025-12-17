cf13b8ea7139b43c7d17bfbc3a526e027d25e6ea
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2025-12-18 00:34:11 +0100

    Added Files

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-let myLeads = [];
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
+inputBtn.addEventListener("click", function() {
+    if (inputEl.value) {
+        myLeads.push(inputEl.value)
+        inputEl.value = "" // Clear the input field after saving
+        renderLeads()
+    }
+})
 
-inputBtn.addEventListener("click",function(){
-    console.log("Button clicked from addEventListener");
-}) 
-{
-    console.log("Button clicked from onclick attribute");
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // Corrected the template string/concatenation logic
+        listItems += `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
+        </li>
+    `
+    }
+    // Added this line to update the DOM
+    ulEl.innerHTML = listItems
+}
