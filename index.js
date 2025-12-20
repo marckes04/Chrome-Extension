c3427a8842c26ce73ca5bea0eb2664f58e75f174
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2025-12-20 02:28:18 +0100

    Added Files

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 let myLeads = []
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+
+//localStorage.setItem("myName", "Per Harald Borgen")
+
+//let name = localStorage.getItem("myName")
+localStorage.clear()
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 inputBtn.addEventListener("click", function() {
     if (inputEl.value) {
         myLeads.push(inputEl.value)
         inputEl.value = "" // Clear the input field after saving
+
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
         renderLeads()
     }
 })
